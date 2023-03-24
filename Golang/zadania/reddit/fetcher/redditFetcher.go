42b33742f656d8a0cf314c274ea02aa395c4e36b
joohnes <kubapienkowski@gmail.com>
2023-03-24 17:51:25 +0100

    poprawiono zadanie

--- Golang/zadania/reddit/fetcher/redditFetcher.go
+++ Golang/zadania/reddit/fetcher/redditFetcher.go
@@ -1,10 +1,14 @@
 package fetcher
 
 import (
+	"encoding/json"
 	"io"
+	"net/http"
+	"strings"
+	"time"
 )
 
-type response struct {
+type Response struct {
 	Data struct {
 		Children []struct {
 			Data struct {
@@ -18,4 +22,43 @@ type response struct {
 type RedditFetcher interface {
 	Fetch() error
 	Save(io.Writer) error
+}
+
+func (f *Response) Save(writer io.Writer) error {
+
+	var str strings.Builder
+	str.WriteString("Date created: " + time.Now().Format(time.DateTime) + "\n\n")
+
+	for _, i := range f.Data.Children {
+		str.WriteString(i.Data.Title + "\n" + i.Data.URL + "\n\n")
+	}
+
+	_, err := writer.Write([]byte(str.String()))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *Response) Fetch(url string) error {
+	client := http.Client{Timeout: time.Second * 10}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return err
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- Golang/zadania/reddit/main.go
+++ Golang/zadania/reddit/main.go
@@ -1,66 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"io"
 	"log"
-	"net/http"
 	"os"
-	"strings"
-	"time"
+	"reddit/fetcher"
 )
-
-type fetcher struct {
-	Data struct {
-		Children []struct {
-			Data struct {
-				Title string `json:"title"`
-				URL   string `json:"url"`
-			} `json:"data"`
-		} `json:"children"`
-	} `json:"data"`
-}
-
-func (f *fetcher) Save(writer io.Writer) error {
-
-	var str strings.Builder
-	str.WriteString("Date created: " + time.Now().Format(time.DateTime) + "\n\n")
-
-	for _, i := range f.Data.Children {
-		str.WriteString(i.Data.Title + "\n" + i.Data.URL + "\n\n")
-	}
-
-	_, err := writer.Write([]byte(str.String()))
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (f *fetcher) Fetch(url string) error {
-	client := http.Client{Timeout: time.Second * 10}
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return err
-	}
-	res, err := client.Do(req)
-	defer res.Body.Close()
-
-	body, readError := io.ReadAll(res.Body)
-	if readError != nil {
-		return errors.New(readError.Error())
-	}
-	jsonErr := json.Unmarshal(body, &f)
-	if jsonErr != nil {
-		return jsonErr
-	}
-	return nil
-}
 
 func main() {
 	const url string = "https://www.reddit.com/r/golang.json"
-	f := fetcher{}
+	f := fetcher.Response{}
 
 	//Fetching data
 	err := f.Fetch(url)
